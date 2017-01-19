3fe0dc812e33bc734d181df5e441a086feed64c0
Midhet Đulović <djulovic_m@hotmail.com>
2017-01-19 18:32:56 +0100

    Test the DELETE /todos/:id route

--- server/server.js
+++ server/server.js
@@ -58,7 +58,7 @@ app.delete('/todos/:id', (req, res)=>{
   if (!todo) {
    return res.status(404).send();
   }
-  return res.status(200).send(todo);
+  return res.status(200).send({todo});
 }).catch((e)=>{
   return res.status(400).send();
 });
