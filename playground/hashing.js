403d03cfc68d1863efcb07a90359fcda39f7995e
Midhet Đulović <djulovic_m@hotmail.com>
2017-01-22 19:02:54 +0100

    Hash user password before saving

--- playground/hashing.js
+++ playground/hashing.js
@@ -1,16 +1,28 @@
 const {SHA256}=require('crypto-js');
 const jwt=require('jsonwebtoken');
+const bcrypt=require('bcryptjs');
 
-var data={
-  id:10
-};
+var password='123abc';
+// bcrypt.genSalt(10, (err, salt)=>{
+// bcrypt.hash(password, salt, (err, hash)=>{
+//   console.log(hash);
+// });
+// });
 
-
-
-var token=jwt.sign(data, '123abc');
-console.log(token);
-var decoded =jwt.verify(token, '123abc');
-console.log('decoded ',decoded);
+var hashedPasswor='$2a$10$6BWtIDkD/dlEOCYdGDskteCQQOmSh18ZUxI2b0GsDsU.REsAqd1PW';
+bcrypt.compare(password, hashedPasswor, (err, res)=>{
+console.log(res);
+});
+// var data={
+//   id:10
+// };
+//
+//
+//
+// var token=jwt.sign(data, '123abc');
+// console.log(token);
+// var decoded =jwt.verify(token, '123abc');
+// console.log('decoded ',decoded);
 /*
 var message='I am user number 3';
 var hash= SHA256(message).toString();
